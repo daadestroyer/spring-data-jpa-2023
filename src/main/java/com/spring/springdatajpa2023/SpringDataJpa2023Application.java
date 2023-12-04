d80e83c748c57bb0657bb33b18c0753fe639c5fa
Shubham Nigam <48306820+daadestroyer@users.noreply.github.com>
2023-12-04 20:52:18 +0530

    project setup done...

---
+++ src/main/java/com/spring/springdatajpa2023/SpringDataJpa2023Application.java
@@ -0,0 +1,13 @@
+package com.spring.springdatajpa2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpa2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpa2023Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/springdatajpa2023/SpringDataJpa2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.springdatajpa2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpa2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
